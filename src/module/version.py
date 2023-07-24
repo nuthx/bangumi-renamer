62b9b09eeaf7542829f0a6d2a6573da3ea2276e6
NuthX <huermos@gmail.com>
2023-07-24 12:57:59 +0800

    更新版本号

--- src/module/version.py
+++ src/module/version.py
@@ -2,12 +2,12 @@ import requests
 
 
 def currentVersion():
-    current_version = "1.0"
+    current_version = "1.1"
     return current_version
 
 
 def latestVersion():
-    url = "https://raw.githubusercontent.com/nuthx/bangumi-renamer/dev_1.1_update/build.spec"
+    url = "https://raw.githubusercontent.com/nuthx/bangumi-renamer/main/build.spec"
     response = requests.get(url)
     response_text = response.text.split('\n')
 
